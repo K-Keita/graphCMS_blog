df5bb487eff3da6044681efee7ad06ebd7bd3185
kitagawakeita <k.syarin610@gmail.com>
2021-08-27 11:00:27 +0900

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider, HttpLink, InMemoryCache } from "@apollo/client";
+import client from "../libs/apollo-client";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+
+  <ApolloProvider client={client}>
+  <Component {...pageProps} />
+  </ApolloProvider>
+  )
 }
 export default MyApp
