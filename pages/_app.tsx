84e8aafd4025c4279eb71689bc4317904083e705
kitagawakeita <k.syarin610@gmail.com>
2021-08-27 11:01:48 +0900

    不必要なものの削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,5 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ApolloProvider, HttpLink, InMemoryCache } from "@apollo/client";
-import client from "../libs/apollo-client";
-
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
